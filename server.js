29d01b07f98961463be6d9c4a6cd2b2f4f3dee0c
fengchunsun <252100760@qq.com>
2018-01-12 23:21:28 +0800

    bb

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
  * Created by feng on 2017/12/28.
  */
 
-
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
@@ -24,10 +23,8 @@ let server = http.createServer(
     let pathName = urlObj.pathname;
     console.log(urlObj,'urlObj');
 
-
     var suffix = pathName.substr(req.url.length - 4, 4);
     var realpath = __dirname + '/';
-
 
     /*app.get('/',function (req,res) {
       res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -35,20 +32,20 @@ let server = http.createServer(
     });*/
 
     if (suffix === '.css') {
-      var filename = pathName.substr(5,req.url.length - 5);
-     // console.log(filename,'css-filename');
+      var filename = pathName.substr(5,pathName.length - 5);
+      console.log(filename,'css-filename');
 
       res.writeHead(200, { 'Content-Type': 'text/css' });
       res.end(get_file_content(realpath + '/css/' + filename));
     } else if (suffix === '.jpg') {
-      var filename = pathName.substr(8,req.url.length - 8);
+      var filename = pathName.substr(8,pathName.length - 8);
       //console.log(filename,'jpg-filename');
 
       res.writeHead(200, {'Content-Type': 'image/jpeg'});
       res.end(get_file_content(realpath+'/images/'+filename));
 
     } else if (suffix.indexOf('.js') != '-1'){
-      var filename = pathName.substr(4,req.url.length - 4);
+      var filename = pathName.substr(4,pathName.length - 4);
       //console.log(filename,'js-filename');
 
       res.writeHead(200, { 'Content-Type': 'application/x-javascript' });
@@ -59,14 +56,20 @@ let server = http.createServer(
     } else if(/\/(\?userId=[\s\S]*)+/.test(req.url)){
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(get_file_content(__dirname + '/' + 'home.html'));
-    }else if(req.url == '/detail.html'){
+    }else if(pathName == '/detail.html'){
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(get_file_content(__dirname + '/' + 'detail.html'));
     }else if(/\/detail\.html\/(\?goodsId=[\s\S]*)+/.test(req.url)){
       res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(get_file_content(__dirname + '/' + 'home.html'));
+      res.end(get_file_content(__dirname + '/' + 'detail.html'));
+    }else if(pathName == '/order.html'){
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(get_file_content(__dirname + '/' + 'order.html'));
+    }else if(/\/order\.html\/(\?goodsId=[\s\S]*)+/.test(req.url)){
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(get_file_content(__dirname + '/' + 'order.html'));
     } else if (suffix == 'html'){
-      var filename = pathName.substr(1,req.url.length - 1);
+      var filename = pathName.substr(1,pathName.length - 1);
       console.log(filename,'html-filename');
 
       res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -79,8 +82,6 @@ let server = http.createServer(
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end('貌似迷路了，，，');
     }
-
-
 
 
     /*res.writeHead(200,{"content-type":"text/html"});
